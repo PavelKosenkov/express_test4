766a8f673dc6af6eaa0765c53cec7b82abf4fcd0
Pavel Kasiankou <p.kasiankou@tio.nl>
2018-03-01 08:15:34 +0100

    azure queue

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
+var azure = require('azure-storage');
+
 var router = express.Router();
+var queueSvc = azure.createQueueService(
+  'functtionapp1856d',
+  'C2BSI+uQH6IUJsWIN7f/dWrHch25V8NCwgHMfkJ1t9nzX053ZX+Ra2+DmxZPwqlkpDJzWmLeclvmXyt5mQ3taw==',
+  'https://functtionapp1856d.queue.core.windows.net/'
+);
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -7,7 +14,12 @@ router.get('/', function(req, res) {
 });
 
 router.post('/', function (req, res) {
-  res.render('index', { 'title': 'Express1', 'input1': req.body.input1 });
+  queueSvc.createMessage('taskqueue1', req.body.input1.toString(), function(error) {
+    if (!error) {
+      res.end(error);
+    }
+  });
+  res.render('index', { 'title': 'Express1', 'input1': payload });  
 });
 
 module.exports = router;
